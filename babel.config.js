06e1170f89756c15493b239d722f186951c0a00a
microsaurs <microsaurs@gamil.com>
2025-03-18 16:23:31 +0900

    기본 환경 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@screens': './src/screens/index',
+          '@components': './src/components/index',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@type': './src/types',
+          '@api': './src/api',
+          '@images': './src/assets/images',
+          '@css': './src/assets/css',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
